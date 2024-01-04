928f2886ab0520888b5f02cb105d60d7a6ff8bfc
KhotsoSelading <k.selading007@gmail.com>
2024-01-04 17:19:59 +0200

    Task 3 - Khotso

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    // Currency class attributes
+    this._code = code;
+    this._name = name;
+  }
+
+  // get and set method for code
+  get code() {
+    return this._code;
+  }
+
+  set code(n) {
+    this._code = n;
+  }
+
+  // get and set method for name
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  //method for displayFullCurrency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
