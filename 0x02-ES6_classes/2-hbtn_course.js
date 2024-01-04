7f7d37ac2d13c9e09d1940dff6ff0d7fd2594c88
KhotsoSelading <k.selading007@gmail.com>
2024-01-04 16:58:41 +0200

    Task 2 - Khotso

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,55 @@
-export default class HolbertonCourse {
+/* eslint-disable no-unused-vars */
+
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._validateAndSet('name', name, 'string');
-    this._validateAndSet('length', length, 'number');
-    this._validateAndSet('students', students, 'array');
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new Error('Students must be an array of strings');
+    }
+
+    // Initializes the c;ass attributes
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
-  //get methods
+  // Get methods
   get name() {
     return this._name;
-  }
-
-  set name(value) {
-    this._validateAndSet('name', value, 'string');
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    this._validateAndSet('length', value, 'number');
-  }
-
-  //set methods
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    this._validateAndSet('students', value, 'array');
+  // set methods
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = value;
   }
 
-  _validateAndSet(attribute, value, type) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attribute} must be a ${type}`);
-    } else {
-      this[`_${attribute}`] = value;
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Length must be a number');
     }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new Error('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
