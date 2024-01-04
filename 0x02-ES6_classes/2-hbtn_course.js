070aee78289b9734f6ac643f42b2806dbe2eda99
KhotsoSelading <k.selading007@gmail.com>
2024-01-04 16:43:12 +0200

    Task 2 - Khotso

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._validateAndSet('name', name, 'string');
+    this._validateAndSet('length', length, 'number');
+    this._validateAndSet('students', students, 'array');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._validateAndSet('name', value, 'string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._validateAndSet('length', value, 'number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._validateAndSet('students', value, 'array');
+  }
+
+  _validateAndSet(attribute, value, type) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a ${type}`);
+    } else {
+      this[`_${attribute}`] = value;
+    }
+  }
+}
