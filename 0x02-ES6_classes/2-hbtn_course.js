17b8ea3c52872c1467226d83013470cd661e8490
KhotsoSelading <k.selading007@gmail.com>
2024-01-04 17:01:03 +0200

    Task 2 - Khotso

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,16 +2,6 @@
 
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new Error('Students must be an array of strings');
-    }
-
     // Initializes the c;ass attributes
     this._name = name;
     this._length = length;
