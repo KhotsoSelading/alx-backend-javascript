9c8ce646855cdf40966859f0b89bba8e7009339c
KhotsoSelading <k.selading007@gmail.com>
2024-01-09 07:04:50 +0200

    Task 5 - Khotso

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,9 +3,9 @@ export default function createInt8TypedArray(length, position, value) {
     throw Error('Position outside range');
   }
 
-  const newBuffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(newBuffer, 0, length);
+  const arrBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(arrBuffer, 0, length);
   int8.set([value], position);
 
-  return new DataView(newBuffer);
+  return new DataView(arrBuffer);
 }
